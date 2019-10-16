6069de2f2643a0f8f364e4d0bd8afd13e61d5a92
Jonathan Dandois <jon.dandois@gmail.com>
2019-10-16 15:47:32 -0400

    trying to get heroku to build

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', function (req, res) {
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") { port = 8000; }
+if (port == null || port == "") { 
+    port = 8000; 
+}
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
